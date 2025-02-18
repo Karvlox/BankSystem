b5510cad17468398542981690a0afebe850a98bc
karvlox <135563580+Karvlox@users.noreply.github.com>
2025-02-18 16:37:47 -0400

    POP3

---
+++ POP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MailKit.Net.Pop3;
+using MimeKit;
+
+class Program
+{
+    static void Main()
+    {
+        string host = "pop.gmail.com";
+        int port = 995;
+        string user = "";
+        string pass = "";
+
+        using (var client = new Pop3Client())
+        {
+            client.Connect(host, port, true);
+            client.Authenticate(user, pass);
+
+            int messageCount = client.Count;
+            Console.WriteLine($"Total de correos: {messageCount}");
+
+            for (int i = 0; i < messageCount; i++)
+            {
+                MimeMessage message = client.GetMessage(i);
+                Console.WriteLine($"De: {message.From}");
+                Console.WriteLine($"Asunto: {message.Subject}");
+                Console.WriteLine($"Contenido: {message.TextBody}");
+            }
+
+            client.Disconnect(true);
+        }
+    }
+}
